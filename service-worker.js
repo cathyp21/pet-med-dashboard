6cea6459845e1f281ff15a39cee26d4e70540c81
cathyp21 <cpoe21@outlook.com>
2026-07-11 18:56:41 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,8 +2,10 @@ const CACHE_NAME = "pet-med-dashboard-v1";
 
 self.addEventListener("install", event => {
   console.log("Service worker installed");
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   console.log("Service worker activated");
+  event.waitUntil(clients.claim());
 });
